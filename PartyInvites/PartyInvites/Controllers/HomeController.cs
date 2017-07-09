c1882683ecff7f9bb5deabb6a939eaf9d851ccf1
plamenti <plamenti_@abv.bg>
2017-07-09 09:33:15 +0300

    Added get and post action methods for Rsvp Form. Added Thanks view

--- PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PartyInvites.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -17,9 +18,17 @@ namespace PartyInvites.Controllers
             return View();
         }
 
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            //TODO: Email response to the party organizer
+            return View("Thanks", guestResponse);
         }
     }
 }
