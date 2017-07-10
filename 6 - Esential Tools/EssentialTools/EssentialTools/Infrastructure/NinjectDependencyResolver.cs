fd546f51792587bcbfb6eec5193ab783b8163355
plamenti <plamenti_@abv.bg>
2017-07-10 08:47:40 +0300

    Chapter 6 - Created Dependency Resolver

---
+++ 6 - Esential Tools/EssentialTools/EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,34 @@
+﻿using EssentialTools.Models;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace EssentialTools.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AppBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AppBindings()
+        {
+            kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+        }
+    }
+}
