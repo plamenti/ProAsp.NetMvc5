b52675ed3df49efda7a4a6966877b3d7af018807
plamenti <plamenti_@abv.bg>
2017-07-10 22:17:25 +0300

    Chapter 6 - Added MinimumDiscountHelper class. Added Unit test project.

---
+++ 6 - Esential Tools/EssentialTools/EssentialTools.Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTools.Models;
+
+namespace EssentialTools.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Discount_Above_100()
+        {
+            // arrange
+            IDiscountHelper target = GetTestObject();
+            decimal total = 200;
+
+            // act
+            var discountedTotal = target.ApplyDiscount(total);
+
+            // assert
+            Assert.AreEqual(total * 0.9M, discountedTotal);
+        }
+
+        private IDiscountHelper GetTestObject()
+        {
+            return new MinimumDiscountHelper();
+        }
+    }
+}
