adf5ffe9f642e7083352eee7286407f87fced73e
plamenti <plamenti_@abv.bg>
2017-07-10 09:53:56 +0300

    Chapter 6 - Added Demo for setting the Object scope - default, creates a new object for every each dependency that is resolved.

--- 6 - Esential Tools/EssentialTools/EssentialTools/Controllers/HomeController.cs
+++ 6 - Esential Tools/EssentialTools/EssentialTools/Controllers/HomeController.cs
@@ -36,7 +36,7 @@ namespace EssentialTools.Controllers
             }
         };
 
-        public HomeController(IValueCalculator calcParam)
+        public HomeController(IValueCalculator calcParam, IValueCalculator calc2)
         {
             calc = calcParam;
         }
