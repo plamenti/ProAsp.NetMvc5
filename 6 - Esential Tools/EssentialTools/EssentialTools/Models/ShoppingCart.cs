dd473f02aab1594fcad5008203f3e386efdebf55
plamenti <plamenti_@abv.bg>
2017-07-10 07:53:55 +0300

    Chapter 6 - Added ShoppingCart model

---
+++ 6 - Esential Tools/EssentialTools/EssentialTools/Models/ShoppingCart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace EssentialTools.Models
+{
+    public class ShoppingCart
+    {
+        private LinqValueCalculator calc;
+
+        public ShoppingCart(LinqValueCalculator calcParam)
+        {
+            this.calc = calcParam;
+        }
+
+        public IEnumerable<Product> Products { get; set; }
+
+        public decimal CalculateProductTotal()
+        {
+            return this.calc.ValueProducts(Products);
+        }
+    }
+}
