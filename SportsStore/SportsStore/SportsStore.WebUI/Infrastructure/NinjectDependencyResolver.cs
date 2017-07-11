fc966dd432e10e010ac7b45ad78f27ceaa03bb14
plamenti <plamenti_@abv.bg>
2017-07-11 10:04:02 +0300

    SportsStore - Set up the DI Container in SportsStore.WebUI

--- SportsStore/SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore/SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using Ninject;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using Ninject;
 
 namespace SportsStore.WebUI.Infrastructure
 {
