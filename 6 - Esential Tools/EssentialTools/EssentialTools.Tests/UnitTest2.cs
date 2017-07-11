4ca59605ca6636e08fee01dd7a0c75a11d98d479
plamenti <plamenti_@abv.bg>
2017-07-11 08:16:16 +0300

    Chapter 6 - IDiscountHelper is mocked

--- 6 - Esential Tools/EssentialTools/EssentialTools.Tests/UnitTest2.cs
+++ 6 - Esential Tools/EssentialTools/EssentialTools.Tests/UnitTest2.cs
@@ -1,6 +1,7 @@
 ﻿using EssentialTools.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
+using Moq;
 
 namespace EssentialTools.Tests
 {
@@ -39,8 +40,10 @@ namespace EssentialTools.Tests
         public void Sum_Products_Correctly()
         {
             // arrange
-            var discounter = new MinimumDiscountHelper();
-            var target = new LinqValueCalculator(discounter);
+            Mock<IDiscountHelper> mock = new Mock<IDiscountHelper>();
+            mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>())).Returns<decimal>(total => total);
+
+            var target = new LinqValueCalculator(mock.Object);
             var goalTotal = products.Sum(p => p.Price);
 
             // act
@@ -48,6 +51,44 @@ namespace EssentialTools.Tests
 
             // assert
             Assert.AreEqual(goalTotal, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentOutOfRangeException))]
+        public void Pass_Through_Variable_Discounts()
+        {
+            // arrange
+            Mock<IDiscountHelper> mock = new Mock<IDiscountHelper>();
+            mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>())).Returns<decimal>(total => total);
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(v => v < 0))).Throws<System.ArgumentOutOfRangeException>();
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(v => v > 100))).Returns<decimal>(total => total * 0.9M);
+
+            //Both are equivalent
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(v => v >= 10 && v <= 100))).Returns<decimal>(total => total - 5);
+            mock.Setup(m => m.ApplyDiscount(It.IsInRange<decimal>(10, 100, Range.Inclusive))).Returns<decimal>(total => total - 5);
+
+            var target = new LinqValueCalculator(mock.Object);
+
+            // act
+            decimal FiveDollarDiscount = target.ValueProducts(createProduct(5));
+            decimal TenDollarDiscount = target.ValueProducts(createProduct(10));
+            decimal FiftyDollarDiscount = target.ValueProducts(createProduct(50));
+            decimal HundredDollarDiscount = target.ValueProducts(createProduct(100));
+            decimal FiveHundredDollarDiscount = target.ValueProducts(createProduct(500));
+            decimal ZeroDollarDiscount = target.ValueProducts(createProduct(0));
+
+            // assert
+            Assert.AreEqual(5, FiveDollarDiscount, "$5 Fail");
+            Assert.AreEqual(5, TenDollarDiscount, "$10 Fail");
+            Assert.AreEqual(45, FiftyDollarDiscount, "$50 Fail");
+            Assert.AreEqual(95, HundredDollarDiscount, "$100 Fail");
+            Assert.AreEqual(450, FiveHundredDollarDiscount, "$500 Fail");
+            target.ValueProducts(createProduct(-1));
+        }
+
+        private Product[] createProduct(decimal price)
+        {
+            return new[] { new Product { Price = price } };
         }
     }
 }
