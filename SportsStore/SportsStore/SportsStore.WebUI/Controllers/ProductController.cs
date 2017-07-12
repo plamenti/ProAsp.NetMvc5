fdca28d224b136bf9d9c59725eb01c30657e28b8
plamenti <plamenti_@abv.bg>
2017-07-12 08:15:53 +0300

    SportsStore.WebUI -> Added pagination

--- SportsStore/SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore/SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -1,20 +1,26 @@
 ﻿using SportsStore.Domain.Abstract;
 using System.Web.Mvc;
+using System.Linq;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository productRepository)
         {
             repository = productRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            return View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize)
+                );
         }
     }
 }
