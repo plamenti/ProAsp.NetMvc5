f7086c20d0e79a004d46310a8de569df488abeb3
plamenti <plamenti_@abv.bg>
2017-07-12 08:00:11 +0300

    SportStore.Domain -> Added EntityFramework, added EFDbContext class and EFPrductRepository class; SportsStore.WebUI ->Added EntityFramework, in NinjectDependencyResolver added binding for IProductRepository to EFProducRepository; In (LocalDB)\MSSQLLocalDB created table Products

--- SportsStore/SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore/SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,6 +1,7 @@
 ﻿using Moq;
 using Ninject;
 using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -31,25 +32,28 @@ namespace SportsStore.WebUI.Infrastructure
         private void AddBindings()
         {
             // put bindings here
-            Mock<IProductRepository> mock = new Mock<IProductRepository>();
 
-            mock.Setup(m => m.Products).Returns(new List<Product>
-            {
-                new Product
-                {
-                    Name = "Football", Price = 25
-                },
-                new Product
-                {
-                    Name = "Surf board", Price = 179
-                },
-                new Product
-                {
-                    Name="Running shoes", Price = 95
-                }
-            });
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
 
-            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            //Mock<IProductRepository> mock = new Mock<IProductRepository>();
+
+            //mock.Setup(m => m.Products).Returns(new List<Product>
+            //{
+            //    new Product
+            //    {
+            //        Name = "Football", Price = 25
+            //    },
+            //    new Product
+            //    {
+            //        Name = "Surf board", Price = 179
+            //    },
+            //    new Product
+            //    {
+            //        Name="Running shoes", Price = 95
+            //    }
+            //});
+
+            //kernel.Bind<IProductRepository>().ToConstant(mock.Object);
         }
     }
 }
