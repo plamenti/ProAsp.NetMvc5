df2807051578b5804e1c124df3098ad10e0d7062
plamenti <plamenti_@abv.bg>
2017-07-13 20:08:56 +0300

    SportsStore.UnitTests - > Added Can_Add_Quantity_For_Existing_Lines test

--- SportsStore/SportsStore/SportsStore.UnitTests/CartTests.cs
+++ SportsStore/SportsStore/SportsStore.UnitTests/CartTests.cs
@@ -28,5 +28,29 @@ namespace SportsStore.UnitTests
             Assert.AreEqual(p1, results[0].Product);
             Assert.AreEqual(p2, results[1].Product);
         }
+
+        [TestMethod]
+        public void Can_Add_Quantity_For_Existing_Lines()
+        {
+            // Arrange
+            // create some test products
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+
+            // Arrange
+            // create a new cart
+            Cart target = new Cart();
+
+            // Act
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 10);
+            CartLine[] results = target.Lines.OrderBy(x => x.Product.ProductID).ToArray();
+
+            // Assert
+            Assert.AreEqual(2, results.Length);
+            Assert.AreEqual(11, results[0].Quantity);
+            Assert.AreEqual(1, results[1].Quantity);
+        }
     }
 }
