e3cdab76ea3d80e7c342f60833dbcaec9b5c057a
plamenti <plamenti_@abv.bg>
2017-07-13 20:03:23 +0300

    SportsStore.UnitTests -> Added CartTests class, added Can_Add_New_Lines unit test.

---
+++ SportsStore/SportsStore/SportsStore.UnitTests/CartTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Entities;
+using System.Linq;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class CartTests
+    {
+        [TestMethod]
+        public void Can_Add_New_Lines()
+        {
+            // Arrange
+            // create some tests products
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+
+            // create a new cart
+            Cart target = new Cart();
+
+            // Act
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            CartLine[] results = target.Lines.ToArray();
+
+            // Assert
+            Assert.AreEqual(2, results.Length);
+            Assert.AreEqual(p1, results[0].Product);
+            Assert.AreEqual(p2, results[1].Product);
+        }
+    }
+}
