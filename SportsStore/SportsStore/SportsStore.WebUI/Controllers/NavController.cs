3b5c9e2ef55dd4bc895cbb4c721ff6ca31ec50bd
plamenti <plamenti_@abv.bg>
2017-07-15 11:13:52 +0300

    SportsStore.WebUI -> Removed Men and HorizontalMenu views, added FlexMEnu.

--- SportsStore/SportsStore/SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore/SportsStore/SportsStore.WebUI/Controllers/NavController.cs
@@ -14,7 +14,7 @@ namespace SportsStore.WebUI.Controllers
             repository = repo;
         }
 
-        public PartialViewResult Menu(string category = null, bool horizontalLayout = false)
+        public PartialViewResult Menu(string category = null)
         {
             ViewBag.SelectedCategry = category;
 
@@ -23,9 +23,7 @@ namespace SportsStore.WebUI.Controllers
                                             .Distinct()
                                             .OrderBy(x => x);
 
-            string viewName = horizontalLayout ? "MenuHorizontal" : "Menu";
-
-            return PartialView(viewName, categories);
+            return PartialView("FlexMenu", categories);
         }
     }
 }
