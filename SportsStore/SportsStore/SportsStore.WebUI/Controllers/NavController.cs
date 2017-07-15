2fcebc7503475a45c8a6e094e65b692acc8ff385
plamenti <plamenti_@abv.bg>
2017-07-15 10:55:16 +0300

    SportStore.WebUI -> Added responsive design fetures.

--- SportsStore/SportsStore/SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore/SportsStore/SportsStore.WebUI/Controllers/NavController.cs
@@ -14,7 +14,7 @@ namespace SportsStore.WebUI.Controllers
             repository = repo;
         }
 
-        public PartialViewResult Menu(string category = null)
+        public PartialViewResult Menu(string category = null, bool horizontalLayout = false)
         {
             ViewBag.SelectedCategry = category;
 
@@ -23,7 +23,9 @@ namespace SportsStore.WebUI.Controllers
                                             .Distinct()
                                             .OrderBy(x => x);
 
-            return PartialView(categories);
+            string viewName = horizontalLayout ? "MenuHorizontal" : "Menu";
+
+            return PartialView(viewName, categories);
         }
     }
 }
