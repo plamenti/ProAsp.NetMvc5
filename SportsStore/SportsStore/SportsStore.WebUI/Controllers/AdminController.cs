eafa42a29d2d8965045fe5ddf852a5aad915ca44
plamenti <plamenti_@abv.bg>
2017-07-21 08:01:50 +0300

    SportsStore.WebUI -> Added AdminController

---
+++ SportsStore/SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        // GET: Admin
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
