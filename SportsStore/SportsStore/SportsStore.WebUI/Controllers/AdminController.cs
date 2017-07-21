e7078713d417117d060d7b9ae493cf67932925f6
plamenti <plamenti_@abv.bg>
2017-07-21 11:06:25 +0300

    SportsStore.WebUI -> Added post Edit action in Admin controller.

--- SportsStore/SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore/SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
@@ -29,5 +29,22 @@ namespace SportsStore.WebUI.Controllers
 
             return View(product);
         }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveProduct(product);
+                TempData["message"] = string.Format("{0} has been saved", product.Name);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // there is something wrong with the data values
+                return View(product);
+            }
+        }
     }
 }
