96f5ff768ef1889218c7faddd7074a9732d787dd
plamenti <plamenti_@abv.bg>
2017-07-22 10:32:56 +0300

    SportsStore.WebUI -> Added Create action in AdminController, changed the form in Edit View to invoke the Edit action in AdminController.

--- SportsStore/SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore/SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
@@ -1,9 +1,6 @@
 ﻿using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace SportsStore.WebUI.Controllers
@@ -45,6 +42,11 @@ namespace SportsStore.WebUI.Controllers
                 // there is something wrong with the data values
                 return View(product);
             }
+        }
+
+        public ViewResult Create()
+        {
+            return View("Edit", new Product());
         }
     }
 }
