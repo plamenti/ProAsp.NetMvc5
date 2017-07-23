1c75232108385155d0a50d9fa4410c3165846e8c
plamenti <plamenti_@abv.bg>
2017-07-23 08:46:20 +0300

    UrlsAndRoutes -> Added CustomerController.

---
+++ UrlsAndRoutes/UrlsAndRoutes/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            ViewBag.Controller = "Customer";
+            ViewBag.Action = "Index";
+
+            return View("ActionName");
+        }
+
+        public ActionResult List()
+        {
+            ViewBag.Controller = "Customer";
+            ViewBag.Action = "List";
+
+            return View("ActionNamen");
+        }
+    }
+}
