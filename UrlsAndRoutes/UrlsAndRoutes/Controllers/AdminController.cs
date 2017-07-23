5a92b42994f581170b33272796e352e96d01402e
plamenti <plamenti_@abv.bg>
2017-07-23 08:47:41 +0300

    UrlsAndRoutes -> Added AdminController.

---
+++ UrlsAndRoutes/UrlsAndRoutes/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            ViewBag.Controller = "Admin";
+            ViewBag.Action = "Index";
+
+            return View("ActionName");
+        }
+    }
+}
