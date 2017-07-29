7ccc761434324745ad99ce5bdae192a6abde5252
plamenti <plamenti_@abv.bg>
2017-07-29 15:59:16 +0300

    UrlsAndRoutes - Added Admin area, added Home controller t it.

---
+++ UrlsAndRoutes/UrlsAndRoutes/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace UrlsAndRoutes.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
