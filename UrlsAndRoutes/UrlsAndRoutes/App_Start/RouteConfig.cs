af605f3feedd5cc880a8fb9fafa9ee89cfbba880
plamenti <plamenti_@abv.bg>
2017-07-29 16:54:07 +0300

    UrlsAndRout -> Enabled Route avaluation before File-Checking in the RouteConfig. Should be changed applicationhost.config too - search for UrlRoutingModule and set preCondition to empty string. To open this config file start the app, right click on the IISExpress -> shall all applications -> click on teh app -> clic on the link after Config label.

--- UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -14,6 +14,8 @@ namespace UrlsAndRoutes
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.RouteExistingFiles = true;
+
             //routes.Add(new LegacyRoute("~/articles/Windows_3.1_Overview.html", "~/old/.NET_1.0_Class_Library"));
 
             routes.MapRoute("MyRoute", "{controller}/{action}/{id}", new { controller="Home", action = "Index", id=UrlParameter.Optional}, new[] { "UrlsAndRoutes.Controllers"});
