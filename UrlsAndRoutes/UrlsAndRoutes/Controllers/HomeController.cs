07f52f0eef9d0092da647c52392312695a5b0109
plamenti <plamenti_@abv.bg>
2017-07-29 10:29:44 +0300

    UrlsAndRoutes -> Added new action link, new Url action to ActionName view. Added Url action and Url route url, added redirect to action result in Home controller.

--- UrlsAndRoutes/UrlsAndRoutes/Controllers/HomeController.cs
+++ UrlsAndRoutes/UrlsAndRoutes/Controllers/HomeController.cs
@@ -9,7 +9,28 @@ namespace UrlsAndRoutes.Controllers
         {
             ViewBag.Controller = "Home";
             ViewBag.Action = "Index";
+
             return View("ActionName");
+        }
+
+        public ActionResult CustomVariable(string id = "DefaultId")
+        {
+            ViewBag.Controller = "Home";
+            ViewBag.Action = "Index";
+            ViewBag.CustomVariable = id;
+
+            return View();
+        }
+
+        public RedirectToRouteResult MyActionMethod()
+        {
+            string myActionUrl = Url.Action("Index", new { id = "MyId" });
+
+            string myRouteUrl = Url.RouteUrl(new { controller = "Home", action = "Index" });
+
+            // do something with URLs...
+
+            return RedirectToAction("Index");
         }
     }
 }
