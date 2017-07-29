a4b865af9571f11198b8b67c0842a02a48c17d06
plamenti <plamenti_@abv.bg>
2017-07-29 15:53:21 +0300

    UrlsAndRoutes -> Try to implement LegacyRoute, which inherits RoiuteBase, fail to register new rotes in RouteConfig!

--- UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using UrlsAndRoutes.Infrastructure;
 
 namespace UrlsAndRoutes
 {
@@ -12,6 +13,8 @@ namespace UrlsAndRoutes
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.Add(new LegacyRoute("~/articles/Windows_3.1_Overview.html", "~/old/.NET_1.0_Class_Library"));
 
             routes.MapRoute("MyRoute", "{controller}/{action}/{id}", new { controller="Home", action = "Index", id=UrlParameter.Optional});
 
