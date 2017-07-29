5fc3ec746959f2e9cc0bbe95a894ee3aa40415df
plamenti <plamenti_@abv.bg>
2017-07-29 10:51:11 +0300

    UrlsAndRoutes -> Added new Leacy controller. Added new GetLegacyURL view.

---
+++ UrlsAndRoutes/UrlsAndRoutes/Controllers/LegacyController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class LegacyController : Controller
+    {
+        // GET: Lagacy
+        public ActionResult GetLegacyURL(string legacyURL)
+        {
+            return View((object)legacyURL);
+        }
+    }
+}
